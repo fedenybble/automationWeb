e3c293b944bddbe188c228f710ae48a4262d6e61
fedenybble <federico.padin@nybblegroup.com>
2022-10-21 12:45:49 -0300

    Se agrega ejercitacion del dia 21/10/22 con sus validaciones correspondientess.

--- src/test/java/basicSelenium/NewToDo.java
+++ src/test/java/basicSelenium/NewToDo.java
@@ -1,6 +1,8 @@
 package basicSelenium;
 
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -17,6 +19,8 @@ import java.security.Key;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 public class NewToDo {
 
@@ -46,7 +50,8 @@ public class NewToDo {
 
         String mailName = "ejemplo@ejemplo.com";
         String password = "ejemplo";
-
+        String nameToDo = "Nueva tarea";
+        String nameToDoEdit = "Nueva tarea editada";
         // -------------------- LOGIN --------------------
 
         //Click [Login] button
@@ -67,20 +72,19 @@ public class NewToDo {
 
         driver.findElement(By.xpath("//td[text()=\"Fede's project\"]")).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("NewItemContentInput")));
+        wait.until(elementToBeClickable(By.id("NewItemContentInput")));
 
         // -------------------- CREATE TO DO --------------------
 
         driver.findElement(By.id("NewItemContentInput")).click();
-        driver.findElement(By.id("NewItemContentInput")).sendKeys("Nueva tarea");
+        driver.findElement(By.id("NewItemContentInput")).sendKeys(nameToDo);
         driver.findElement(By.id("NewItemAddButton")).click();
 
         driver.manage().timeouts().implicitlyWait(Duration.of(10, ChronoUnit.SECONDS));
 
-
         // -------------------- EDIT TO DO --------------------
 
-        driver.findElement(By.xpath("//div[@class=\"ItemContentDiv\" and text()=\"Nueva tarea\"]")).click();
+        driver.findElement(By.xpath("//div[@class=\"ItemContentDiv\" and text()='"+ nameToDo +"']")).click();
         driver.findElement(By.xpath("//div[@class=\"ItemIndicator\"]//img[@style=\"display: inline;\"]")).click();
 
         driver.manage().timeouts().implicitlyWait(Duration.of(10, ChronoUnit.SECONDS));
@@ -90,20 +94,23 @@ public class NewToDo {
         driver.manage().timeouts().implicitlyWait(Duration.of(10, ChronoUnit.SECONDS));
 
         driver.findElement(By.xpath("//textarea[@id='ItemEditTextbox']")).clear();
-        driver.findElement(By.xpath("//textarea[@id='ItemEditTextbox']")).sendKeys("Nueva tarea editada");
+        driver.findElement(By.xpath("//textarea[@id='ItemEditTextbox']")).sendKeys(nameToDoEdit);
         driver.findElement(By.xpath("//textarea[@id='ItemEditTextbox']")).sendKeys(Keys.RETURN);
 
 
         // -------------------- DELETE TO DO --------------------
 
-        driver.findElement(By.xpath("//div[@class=\"ItemContentDiv\" and text()=\"Nueva tarea editada\"]")).click();
+        driver.findElement(By.xpath("//div[@class=\"ItemContentDiv\" and text()='"+nameToDoEdit+"']")).click();
         driver.findElement(By.xpath("//div[@class=\"ItemIndicator\"]//img[@style=\"display: inline;\"]")).click();
 
         driver.manage().timeouts().implicitlyWait(Duration.of(10, ChronoUnit.SECONDS));
 
         driver.findElement(By.xpath("//ul[@id=\"itemContextMenu\"]//a[@href=\"#delete\"]")).click();
 
+        wait.until(elementToBeClickable(By.xpath("//span[@id='InfoMessageText']")));
+        Assertions.assertEquals("Item has been Deleted",driver.findElement(By.xpath("//span[@id='InfoMessageText']")).getText(),"ERROR no se borro el item");
 
+        Thread.sleep(10000);
 
     }
 
